8b17fe893d7f67ed3e2b3bba61580c188cfceb85
Alejandro <aguerreroegido@gmail.com>
2025-04-15 13:19:56 +0200

    Initialize Spring WebFlux project with Maven wrapper
    Set up a new Spring Boot project using WebFlux with Maven. Added Maven wrapper, dependencies for R2DBC and H2 database, and configured application properties. Included basic application and test setup.

---
+++ src/test/java/com/alejandro/webfluxplayground/WebfluxPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandro.webfluxplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
