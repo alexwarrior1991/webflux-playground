5bbed21dd5cc7893e14fe0e3db62987f7b07aea1
Alejandro <aguerreroegido@gmail.com>
2025-04-20 16:47:54 +0200

    Add Product model and REST controllers for WebFlux and WebMVC
    Introduces the `Product` model and two controllers: `TraditionalWebController` for WebMVC and `ReactiveWebController` for WebFlux. Configures database schema and initializes sample data via `data.sql`. Updates application properties to support new configurations.

--- src/main/java/com/alejandro/webfluxplayground/WebfluxPlaygroundApplication.java
+++ src/main/java/com/alejandro/webfluxplayground/WebfluxPlaygroundApplication.java
@@ -2,8 +2,10 @@ package com.alejandro.webfluxplayground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.alejandro.webfluxplayground.${sec}")
+@EnableR2dbcRepositories(basePackages = "com.alejandro.webfluxplayground.${sec}")
 public class WebfluxPlaygroundApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/alejandro/webfluxplayground/sec01/Product.java
@@ -0,0 +1,4 @@
+package com.alejandro.webfluxplayground.sec01;
+
+public record Product(Integer id, String description, Integer price) {
+}
